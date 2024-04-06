e1bcd3067a1426d16e78befdd58e5e9ab3ff6f27
Anton Kozlov <akozlov@azul.com>
2024-04-06 10:57:12 +0300

    Import

---
+++ src/main/java/com/example/portablecli/PortableCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.portablecli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortableCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortableCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/portablecli/PortableCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portablecli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortableCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
