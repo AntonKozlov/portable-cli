8e749af26fa6c9f594bd5abb970aecf07af9847f
Anton Kozlov <akozlov@azul.com>
2024-04-06 21:03:41 +0300

    Add checkpoint command

--- src/main/java/com/example/portablecli/PortableCliApplication.java
+++ src/main/java/com/example/portablecli/PortableCliApplication.java
@@ -1,13 +1,21 @@
 package com.example.portablecli;
 
+import org.crac.CheckpointException;
+import org.crac.RestoreException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
 
 @SpringBootApplication
+@ShellComponent
 public class PortableCliApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PortableCliApplication.class, args);
 	}
 
+    @ShellMethod(key = "checkpoint")
+    public void checkpoint() throws RestoreException, CheckpointException {
+        org.crac.Core.checkpointRestore();
+    }
 }
